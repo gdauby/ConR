90a9d1b48a63e9591cf2e7251f905afa8eaf3c85
Gilles Dauby <gildauby@gmail.com>
2017-04-28 16:28:02 +0200

    deleting text

---
+++ R/Malagasy_amphibian.R
@@ -0,0 +1,24 @@
+#' Dataset of Malagasy Amphibian occurrences
+#'
+#' A SpatialPolygonDataFrame showing land cover
+#'
+#'
+#' @keywords datasets
+#' @name Malagasy_amphibian
+#' @usage data(Malagasy_amphibian)
+#' @format A dataframe
+#' @source GBIF.org (9th February 2016) GBIF Occurrence Download \url{http://doi.org/10.15468/dl.2tkoae}
+#' @references Download Information:
+#' DOI: http://doi.org/10.15468/dl.2tkoae (may take some hours before being active)
+#' Creation Date: Tuesday, February 9, 2016 9:31:26 PM CET
+#' Records included: 74352 records from 50 published datasets
+#' Data size: 4.6 MB
+#' Download format: SIMPLE_CSV
+#' Filter used: TaxonKey: Amphibia Linnaeus, 1758
+#' Geometry: POLYGON((-17.578125 33.724339,-17.578125 -36.597889,56.953125 -36.597889,56.953125 33.724339,-17.578125 33.724339))
+#' HasCoordinate: true
+#' HasGeospatialIssue: false
+#' BasisOfRecord: Human Observation or Observation or Living Specimen or Literature Occurrence or Specimen
+#' @details This dataset was further filtered out for keeping only species that have at least one occurrence in Madagascar
+NULL
+
---
+++ R/land.R
@@ -0,0 +1,12 @@
+#' land
+#'
+#' A SpatialPolygonDataFrame showing land cover
+#'
+#'
+#' @keywords datasets
+#' @name land
+#' @usage data(land)
+#' @format A dataframe
+#' @source \url{http://www.naturalearthdata.com/downloads/10m-physical-vectors/}
+NULL
+
