f74ca9a17e2115b563167ff02f6cb0bfb7477374
Gilles Dauby <gildauby@gmail.com>
2017-05-09 10:13:09 +0200

    edit comments alpha hull functions

--- R/IUCNeval.functionv11.R
+++ R/IUCNeval.functionv11.R
@@ -20,7 +20,7 @@
 }
 
 
-## The function XXX was originally posted in the website https://casoilresource.lawr.ucdavis.edu/software/r-advanced-statistical-package/working-spatial-data/converting-alpha-shapes-sp-objects/
+## The functions ahull_to_SPLDF and alpha.hull.poly were originally posted in the website https://casoilresource.lawr.ucdavis.edu/software/r-advanced-statistical-package/working-spatial-data/converting-alpha-shapes-sp-objects/
 ## in a now broken link. It is also used in github functions written by David Bucklin, see https://github.com/dnbucklin/r_movement_homerange 
 
 .ahull_to_SPLDF <- function(x, proj4string=NA)
