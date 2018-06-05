e7e2fde82a6e0c731bf874e0b072073c1bb75765
Gilles Dauby <gildauby@gmail.com>
2018-06-05 18:41:22 +0200

    corrections exepected results

--- tests/testthat/test-3.R
+++ tests/testthat/test-3.R
@@ -11,8 +11,8 @@ test_that("IUCN.eval", {
   Results <- IUCN.eval(dataset.ex, country_map=land)
   
   expect_output(str(Results), "data.frame")
-  expect_equal(dim(Results), c(6,9))
+  expect_equal(dim(Results), c(6,10))
   
-  expect_equal(404, Results[1,2])
+  expect_equal(404, Results[1,3])
   
 })
