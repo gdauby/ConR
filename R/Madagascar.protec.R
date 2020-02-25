d2d8bd680e05c2aa57b56bbcef3f57bcd73c7369
gdauby <gildauby@gmail.com>
2020-02-25 09:38:37 +0100

    last version

---
+++ R/Madagascar.protec.R
@@ -0,0 +1,12 @@
+#' Dataset of Malagasy protected areas
+#'
+#' A SpatialPolygonDataFrame showing Malagasy protected areas
+#'
+#'
+#' @keywords datasets
+#' @name Madagascar.protec
+#' @usage data(Madagascar.protec)
+#' @format A SpatialPolygonDataFrame
+#' @source World Database on Protected Areas http://protectedplanet.net/country/MG
+#' @references http://protectedplanet.net/country/MG
+NULL
---
+++ R/dataset.ex.R
@@ -0,0 +1,10 @@
+#' Dataset of plant species distribution
+#'
+#' A dataframe of three columns
+#'
+#'
+#' @keywords datasets
+#' @name dataset.ex
+#' @usage data(dataset.ex)
+#' @format A dataframe
+NULL
