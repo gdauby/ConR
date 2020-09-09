cc42c667ad9650775e5613528d5ae47b4110e2ad
gdauby <gildauby@gmail.com>
2020-09-09 19:38:30 +0200

    multiple updates

--- R/subpop.estimation.R
+++ R/subpop.estimation.R
@@ -2,8 +2,9 @@
 #'
 #' @description Subpopulations estimation
 #' 
-#' @param XY data.frame
-#' @param Resol_sub_pop integer
+#' @param XY data.frame with first two columns are projected coordinates (numeric)
+#' @param Resol_sub_pop numeric. Defines the radius of the circles
+#'   around each occurrence, in kilometres.
 #' @param export_shp logical
 #'
 #' @importFrom utils packageVersion
@@ -25,12 +26,34 @@ subpop.estimation <- function(XY,
   #                  id = 1)
   
   #### GILLES: SOME CODES FROM OTHER FUNCTIONS TO USE sf INSTEAD OF sp and rgeos
-  points_sf <- sf::st_as_sf(XY[,1:3], coords = c("ddlon", "ddlat"))
+  
+  # centroid <- st_as_sf(data.frame(mean_ddlon = mean(XY$ddlon), 
+  #                     mean_ddlat = mean(XY$ddlat)), 
+  #          coords = c("mean_ddlon", "mean_ddlat"))
+  # st_crs(centroid) <- 4326
+  # centroid_proj <- st_transform(centroid, crs = proj_crs(3786))
+  # centroid_proj <- st_coordinates(centroid_proj)
+  # centroid_proj <- 
+  #   rbind(centroid_proj, centroid_proj + Resol_sub_pop * 1000)
+  # centroid_proj <- st_as_sf(as.data.frame(centroid_proj), 
+  #                      coords = c("X", "Y"))
+  # st_crs(centroid_proj) <- proj_crs(3786)
+  # centroid_added <- st_transform(centroid_proj, crs = 4326)
+  
+  # st_distance(centroid_added)
+  # 
+  # dist(st_coordinates(centroid_added))
+  
+  
+  points_sf <- sf::st_as_sf(XY, coords = c(1, 2))
   buff_sf <- sf::st_buffer(points_sf, dist = Resol_sub_pop * 1000)
   buff_sf <- sf::st_union(buff_sf) ### GILLES, WE NEED TO JOIN THE OVERLAPPING CIRCLES, RIGHT? PLEASE CHECK!
+  # buff_sf_multipoly <- sf::st_cast(buff_sf, "MULTIPOLYGON")
   buff_sf <- sf::st_cast(buff_sf, "POLYGON")
   SubPopPoly <-
-    sf::st_as_sf(data.frame(buff_sf, tax = unique(XY$tax)))
+    sf::st_as_sf(data.frame(buff_sf, tax = ifelse(any(colnames(XY) == "tax"), unique(XY$tax), "taxa1")))
+  # SubPopPoly_multipoly <-
+  #   sf::st_as_sf(data.frame(buff_sf_multipoly, tax = ifelse(any(colnames(XY) == "tax"), unique(XY$tax), "taxa1")))
   
   # XY_sf <- 
   #   st_as_sf(XY, coords = c(1, 2))
