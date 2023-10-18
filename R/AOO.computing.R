d97f413286faa2b56b0eadab8876a929afcf7dad
gdauby <gildauby@gmail.com>
2023-10-18 15:43:32 +0100

    clean

--- R/AOO.computing.R
+++ R/AOO.computing.R
@@ -184,7 +184,7 @@ AOO.computing <- function(XY,
       res_df[issue_close_to_anti, 3] <-
       "AOO could not computed because grid cells would overlap with antimeridian"
     
-    if(export_shp) {
+    if (export_shp) {
 
       shapes <- output[names(output) == "spatial"]
       shapes <- do.call('rbind', shapes)
@@ -205,5 +205,35 @@ AOO.computing <- function(XY,
   if(export_shp) 
     return(list(AOO = res_df, 
                 AOO_poly = shapes))
+  
+}
+
+
+
+AOO.estimation <- function(coordEAC,
+                           cell_size = 2,
+                           nbe_rep = 0,
+                           export_shp = FALSE,
+                           proj_type = proj_type
+) {
+  
+  res <-
+    cell.occupied(
+      nbe_rep = nbe_rep,
+      size = cell_size,
+      coord = coordEAC[,c(2, 1)],
+      export_shp = export_shp,
+      proj_type = proj_type
+    )
+  
+  
+  
+  AOO <- res[[2]] * cell_size * cell_size
+  
+  if (export_shp)
+    return(list(AOO = AOO, poly_AOO = res[[1]]))
+  
+  if (!export_shp)
+    return(AOO)
   
 }
